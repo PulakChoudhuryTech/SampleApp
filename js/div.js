5b245524ed6167a07e5e9c2e800106cd1145d1c8
unknown <pulakp@PC1396.itp.objectfrontier.com>
2015-02-18 12:25:30 +0530

    Add all the js files

---
+++ js/div.js
@@ -0,0 +1,6 @@
+//file to be tested
+'use strict';
+
+function div(a, b) {
+    return a / b;
+}
---
+++ test/unit-spec.js
@@ -0,0 +1,9 @@
+//jasmin unit testing file
+
+describe('Sample unit test cases', function () { //test suite
+
+    it('should divide two numbers', function() {       //test case
+        expect(div(6,2)).toBe(3);
+    });
+   
+});
